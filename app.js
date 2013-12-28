cd67f4e7727265c000c462b58b014380999362e0
Jacques Marneweck <jacques@powertrip.co.za>
2013-12-28 11:03:30 +0200

    Use bunyan for logging
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- app.js
+++ app.js
@@ -3,20 +3,43 @@
  * Module dependencies.
  */
 
+var mod_assert = require('assert');
+var mod_bunyan = require('bunyan');
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var LOG = mod_bunyan.createLogger({
+  name: 'abusetool',
+  stream: process.stdout,
+  level: 'info'
+});
+
 var app = express();
 
+app.set('logger', LOG);
+
 // all environments
+//app.set('host', process.env.HOST || '127.0.0.1');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+// bunyan logging
+app.use(function(req, res, next) {
+    var end = res.end;
+    req._startTime = new Date();
+    res.end = function(chunk, encoding){
+        res.end = end;
+        res.end(chunk, encoding);
+        LOG.info({req: req, res: res, total_time: new Date() - req._startTime}, 'handled request/response');
+    };
+
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -34,5 +57,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  LOG.info('Express server listening on port ' + app.get('port'));
 });
