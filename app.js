21c5ad9ada51296bb5763f08528507bfa28168ea
Jacques Marneweck <jacques@powertrip.co.za>
2013-12-29 10:08:34 +0200

    Render whois results
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/whois/:ip', routes.whois);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+/*
+ * GET whois page.
+ */
+exports.whois = function(req, res){
+  var mod_whois = require('node-whois');
+
+  mod_whois.lookup(req.params.ip, function(err, data) {
+    console.log(err, data);
+
+    res.render('whois', { title: 'Whois', ip: req.params.ip, data: data });
+  });
+};
